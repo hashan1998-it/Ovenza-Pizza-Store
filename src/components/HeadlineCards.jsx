0ab74ae638e1b3cc444d005d8b286ba1e416eca6
Hashan Danushka <hashansooriyage@gmail.com>
2024-01-14 18:36:53 +0530

    Created headlinePizza constant

--- src/components/HeadlineCards.jsx
+++ src/components/HeadlineCards.jsx
@@ -1,38 +1,19 @@
-
+import { headlinePizzas } from "../data/constant"
 
 function HeadlineCards() {
     return (
         <div className="max-w-[1640px] p-4 mx-auto grid md:grid-cols-3 gap-6">
             {/* Card */}
-            <div className="relative rounded-xl">
-                <div className="bg-black/40 h-full w-full absolute text-white font-bold rounded-xl p-5 text-lg">
-                    <p>Cheese Pizza</p>
-                    <p>Starting from Rs.980</p>
-                    <button className="bg-green-400 mt-2">Order Now</button>
-                </div>
-                <img src="https://images.unsplash.com/photo-1604917877934-07d8d248d396?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2hlZXNlJTIwcGl6emF8ZW58MHx8MHx8fDA%3D" alt="chesse pizza" className="max-h-[200px] w-full object-cover rounded-xl" />
-            </div>
-
-            {/* Card */}
-            <div className="relative rounded-xl">
-                <div className="bg-black/40 h-full w-full absolute text-white font-bold rounded-xl p-5 text-lg">
-                    <p>Chicken Pizza</p>
-                    <p>Starting from Rs.980</p>
-                    <button className="bg-green-400 mt-2">Order Now</button>
-                </div>
-                <img src="https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2hpY2tlbiUyMHBpenphfGVufDB8fDB8fHww" alt="chesse pizza" className="max-h-[200px] w-full object-cover rounded-xl" />
-            </div>
-
-            {/* Card */}
-            <div className="relative rounded-xl">
-                <div className="bg-black/40 h-full w-full absolute text-white font-bold rounded-xl p-5 text-lg">
-                    <p>Veg Pizza</p>
-                    <p>Starting from Rs.980</p>
-                    <button className="bg-green-400 mt-2">Order Now</button>
-                </div>
-                <img src="https://images.unsplash.com/photo-1669895616443-5d21d5acc6e0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8dmVnJTIwcGl6emF8ZW58MHx8MHx8fDA%3D" alt="chesse pizza" className="max-h-[200px] w-full object-cover rounded-xl" />
-            </div>
-
+            {headlinePizzas.map(item =>
+                <div className="relative rounded-xl" key={item.key}>
+                    <div className="bg-black/40 h-full w-full absolute text-white font-bold rounded-xl p-5 text-lg">
+                        <p>{item.name}</p>
+                        <p>{item.price}</p>
+                        <button className="bg-green-400 mt-2">Order Now</button>
+                    </div>
+                    <img src={item.image} alt="pizza" className="max-h-[200px] w-full object-cover rounded-xl" />
+                </div>)
+            }
         </div>
     )
 }
--- src/data/constant.jsx
+++ src/data/constant.jsx
@@ -4,11 +4,16 @@ import { MdFavoriteBorder } from "react-icons/md";
 import { TbTruckDelivery } from "react-icons/tb";
 
 export const sideDrawerMenuText = [
-    { icon: <TbTruckDelivery className="mr-2" />, text: "Orders" ,key:"Orders"},
-    { icon: <MdFavoriteBorder className="mr-2" />, text: "Favorites",key:"Favorites" },
-    { icon: <IoIosWallet className="mr-2" />, text: "Wallet",key:"Wallet" },
-    { icon: <IoIosHelpCircle className="mr-2" />, text: "Help",key:"Help" },
-    { icon: <AiFillTag className="mr-2" />, text: "Promotions",key:"Promotions" },
-    { icon: <IoIosPersonAdd className="mr-2" />, text: "Friends",key:"Friends" }
+    { icon: <TbTruckDelivery className="mr-2" />, text: "Orders", key: "Orders" },
+    { icon: <MdFavoriteBorder className="mr-2" />, text: "Favorites", key: "Favorites" },
+    { icon: <IoIosWallet className="mr-2" />, text: "Wallet", key: "Wallet" },
+    { icon: <IoIosHelpCircle className="mr-2" />, text: "Help", key: "Help" },
+    { icon: <AiFillTag className="mr-2" />, text: "Promotions", key: "Promotions" },
+    { icon: <IoIosPersonAdd className="mr-2" />, text: "Friends", key: "Friends" }
 ]
 
+export const headlinePizzas = [
+    { key: "Cheese", name: "Cheese Pizza", price: "989", image: "https://images.unsplash.com/photo-1604917877934-07d8d248d396?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2hlZXNlJTIwcGl6emF8ZW58MHx8MHx8fDA%3D" },
+    { key: "Chicken", name: "Chicken Pizza", price: "989", image: "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2hpY2tlbiUyMHBpenphfGVufDB8fDB8fHww" },
+    { key: "Veg", name: "Veg Pizza", price: "989", image: "https://images.unsplash.com/photo-1669895616443-5d21d5acc6e0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8dmVnJTIwcGl6emF8ZW58MHx8MHx8fDA%3D" },
+]
